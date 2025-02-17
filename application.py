b0e0addf97b1bafce2b24cacbf1f50a2deef709f
guidoasbun <guido@asbun.io>
2025-02-16 17:42:00 -0800

    Initial commit

--- application.py
+++ application.py
@@ -1,21 +1,23 @@
 from flask import Flask
 from flask_socketio import SocketIO
+import eventlet
 
+# Import WebSocket handlers from separate file
+from socket_handlers import register_socket_handlers
+
+# Flask app setup
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "your_secret_key"
+app.config['SECRET_KEY'] = 'my_secret_key'
 
+# WebSocket setup
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-@socketio.on("connect")
-def handle_connect():
-    print("Client connected")
-    socketio.emit("server_message", {"message": "Connected to WebSocket!"})
+# Register WebSocket event handlers
+register_socket_handlers(socketio)
 
-@socketio.on("send_message")
-def handle_message(data):
-    message = data.get("message")
-    if message:
-        socketio.emit("receive_message", {"message": message}, broadcast=True)
+@app.route('/')
+def index():
+    return "WebSocket Chat Server Running"
 
-if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=5000)
