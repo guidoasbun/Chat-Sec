223ef933db919248f8d3019fc57ca87152e4a36d
guidoasbun <guido@asbun.io>
2025-02-16 17:17:23 -0800

    Added Elastic Beanstalk configurations

--- application.py
+++ application.py
@@ -1,23 +1,21 @@
 from flask import Flask
 from flask_socketio import SocketIO
-import eventlet
 
-# Import WebSocket handlers from separate file
-from socket_handlers import register_socket_handlers
-
-# Flask app setup
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'my_secret_key'
+app.config["SECRET_KEY"] = "your_secret_key"
 
-# WebSocket setup
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-# Register WebSocket event handlers
-register_socket_handlers(socketio)
+@socketio.on("connect")
+def handle_connect():
+    print("Client connected")
+    socketio.emit("server_message", {"message": "Connected to WebSocket!"})
 
-@app.route('/')
-def index():
-    return "WebSocket Chat Server Running"
+@socketio.on("send_message")
+def handle_message(data):
+    message = data.get("message")
+    if message:
+        socketio.emit("receive_message", {"message": message}, broadcast=True)
 
-if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    socketio.run(app, host="0.0.0.0", port=5000)
